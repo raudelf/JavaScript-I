4198adc6895825986d61c7d97fbafdc28186ecf2
Raudel Flores <rawdell@outlook.com>
2019-10-02 20:35:30 -0500

    completed the MVP for objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,14 +29,18 @@ const kennan = {
   id: 1,
   name: 'Kennan',
   email: 'kdiben1@tinypic.com',
-  gender: 'M'
+  gender: 'M',
+  speak: function (){
+    return `Hello, my name is ${this.name}!`
+  }
 }
 
 const keven = {
   id: 2,
   name: 'Keven',
   email: 'kmummery2@wikimedia.org',
-  gender: 'M'
+  gender: 'M',
+  
 }
 
 const gannie = {
@@ -50,7 +54,10 @@ const antonietta = {
   id: 4,
   name: 'Antonietta',
   email: 'adaine5@samsung.com',
-  gender: 'F'
+  gender: 'F',
+  multiplyNums: function (num1, num2){
+    return num1 * num2
+  }
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
@@ -70,9 +77,11 @@ console.log(antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
